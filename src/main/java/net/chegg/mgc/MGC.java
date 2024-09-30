c93c098c566932271510ea44627fb84844e681d9
Chegg32 <welchmarshfamily@outlook.com>
2024-09-30 11:56:02 -0400

    commit

---
+++ src/main/java/net/chegg/mgc/MGC.java
@@ -0,0 +1,17 @@
+package net.chegg.mgc;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MGC implements ModInitializer {
+	public static final String MOD_ID = "mgc";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/chegg/mgc/MGCClient.java
@@ -0,0 +1,10 @@
+package net.chegg.mgc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MGCClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
